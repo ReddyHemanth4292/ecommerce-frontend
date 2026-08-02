e079fc3242ac615fe853e4da3a70085eafd9c963
ReddyHemanth4292 <rq0665@srmist.edu.in>
2026-08-03 00:46:39 +0530

    Learn React state and useState

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,23 @@
-import QuantitySelector from './QuantitySelector'
+import { useState } from "react";
+import QuantitySelector from "./QuantitySelector";
 function ProductCard({ name, brand, price }) {
-    return (
-        <div>
-            <h3>{name}</h3>
-            <p>{brand}</p>
-            <p>₹{price}</p>
-            <button>View Product</button>
-        </div>
-    );
+  const [quantity, setQuantity] = useState(1);
+  return (
+    <div>
+      <h3>{name}</h3>
+      <p>{brand}</p>
+      <p>₹{price}</p>
+      <div>
+        <button onClick={() => {if(quantity>1){setQuantity(quantity - 1)}}}>-</button>
+
+        <span>{quantity}</span>
+
+        <button onClick={() => {setQuantity(quantity + 1)}}>+</button>
+      </div>
+      <div>Total: ₹{quantity*price}</div>
+      <button>Add to Cart</button>
+    </div>
+  );
 }
 
 export default ProductCard;
