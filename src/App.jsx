415d309d320fa3910475fac619adb01b4e6661b4
ReddyHemanth4292 <rq0665@srmist.edu.in>
2026-08-03 02:25:26 +0530

    Learn React lists and map

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,26 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import ProductCard from './components/ProductCard'
 import Navbar from './components/Navbar'
+import ProductList from './components/ProductList'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [products, setProducts] = useState([
+        {
+          id: 1,
+          name: "iPhone 15",
+          brand: "Apple",
+          description: "Apple smartphone",
+          price: 70000,
+          quantity: 10,
+          sku: "IPH15"
+        }
+    ]);
 
   return (
         <div>
             <Navbar />
-
-            <h1>Welcome to the Store</h1>
-            <ProductCard name="iPhone 15" brand="Apple" price={70000}/>
-            <ProductCard name="Galaxy S24" brand="Samsung" price={65000}/>
-            <ProductCard name="Pixel 9" brand="Google" price={60000}/>
+            <h1>Products</h1>
+            <ProductList products={products} />
         </div>
   )
 }
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import QuantitySelector from "./QuantitySelector";
-function ProductCard({ name, brand, price }) {
+function ProductCard({product}) {
   const [quantity, setQuantity] = useState(1);
   return (
     <div>
-      <h3>{name}</h3>
-      <p>{brand}</p>
-      <p>₹{price}</p>
+      <h3>{product.name}</h3>
+      <p>{product.brand}</p>
+      <p>₹{product.price}</p>
+      <p>SKU: {product.sku}</p>
+      <p>Stock: {product.quantity}</p>
+      <p></p>
       <div>
         <button onClick={() => {if(quantity>1){setQuantity(quantity - 1)}}}>-</button>
 
@@ -14,7 +17,7 @@ function ProductCard({ name, brand, price }) {
 
         <button onClick={() => {setQuantity(quantity + 1)}}>+</button>
       </div>
-      <div>Total: ₹{quantity*price}</div>
+      <div>Total: ₹{quantity*product.price}</div>
       <button>Add to Cart</button>
     </div>
   );
