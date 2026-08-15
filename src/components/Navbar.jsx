c844c789f1accbeb23d67565679f9a1e8a867319
ReddyHemanth4292 <rq0665@srmist.edu.in>
2026-08-15 20:35:57 +0530

    Implement cart page with JWT authentication

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,9 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav className="navbar">
-      <h2> My E-Commerce Store </h2> <Link to="/products"> Products </Link>
+      <h2> My E-Commerce Store </h2>
+      <Link to="/products"> Products </Link>
+      {" | "}
       <Link to="/cart">Cart</Link>
     </nav>
   );
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,38 +1,48 @@
 import { useEffect, useState } from "react";
 import { getCart } from "../services/cartService";
 
-function cart(){
-    const [cart,setCart]= useState(null);
-    const [loading,setLoading]=useState(true);
-    const [error,setError]=useState("");
-    useEffect(()=>{
-        const fetchCart = async()=>{
-            try{
-                const data=await getCart();
-                setCart(data);
-            }catch(error){
-                setError(error.message);
-            }
-            finally{
-                setLoading(false);
-            }
-        };
-        fetchCart();
-    },[]);
-    if(loading){
-        return <p>Loading cart...</p>
-    }
-    if(error){
-        return <p>{error}</p>;
-    }
-    return(
-        <div>
-            <h2>My Cart</h2>
-            <pre>
-                {JSON.stringify(cart, null, 2)}
-            </pre>
+function cart() {
+  const [cart, setCart] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  useEffect(() => {
+    const fetchCart = async () => {
+      try {
+        const data = await getCart();
+        setCart(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCart();
+  }, []);
+  if (loading) {
+    return <p>Loading cart...</p>;
+  }
+  if (error) {
+    return <p>{error}</p>;
+  }
+  if (!cart || cart.items.length == 0) {
+    <p>Your cart is empty</p>;
+  }
+  return (
+    <div>
+      <h1>Shopping Cart</h1>
+      {cart.items.map((item) => (
+        <div key={item.productId}>
+          <h3>{item.productName}</h3>
+          <p>Price: {item.price}</p>
+          <p>Quantity: {item.quantity}</p>
+          <p>Subtotal: ₹{item.subtotal}</p>
         </div>
-    )
+      ))}
+      <hr />
+      <h3>Total Items: {cart.totalItems}</h3>
+      <h2>Total: ₹{cart.totalPrice}</h2>
+    </div>
+  );
 }
 
 export default cart;
